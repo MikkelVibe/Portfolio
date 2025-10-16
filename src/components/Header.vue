c1200f33d6a99cba99dfaa9bc8044ca1cd76f522
Mikkel Vibe Christensen <74587882+MikkelVibe@users.noreply.github.com>
2025-10-16 11:16:07 +0200

    Update GitHub link in Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,7 +22,7 @@
                     </svg>
                 </a>
 
-                <a href="https://github.com/MikkelVibeUCN" target="_blank" rel="noopener noreferrer"
+                <a href="https://github.com/MikkelVibe" target="_blank" rel="noopener noreferrer"
                     aria-label="GitHub Profile" class="icon-link">
                     <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="currentColor"
                         class="icon">
